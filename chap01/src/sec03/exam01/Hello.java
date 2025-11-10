64629e2e8ae1d00391d4ece73574d85480d37cbb
HyeonHo41 <kimhyenho41@gmail.com>
2025-11-10 16:17:34 +0900

    java

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,17 @@
+package sec03.exam01;
+
+//1. 각자 테마 변경 및 글자 크기 조정
+// TODO: Task 뷰 추가해서 확인
+public class Hello {
+	
+    //프로그램 실행 진입점
+	// 자바 프로그램 실행 시 가장 먼저 실행되는 메소드
+	public static void main(String[] args) {//메소드 선언: 
+		// TODO Auto-generated method stub
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+		System.out.print("Hello, // /**/ java");// 문자열 내부에는 주석을 붙일 수 없다.
+
+	}
+
+}
